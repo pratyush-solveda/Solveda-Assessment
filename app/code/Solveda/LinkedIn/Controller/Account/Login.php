33239787bd2be8e55a2cc73587571f1caae3a183
Pratyush <psrivastava@solveda.com>
2024-01-16 17:12:28 +0530

    controller changes

--- app/code/Solveda/LinkedIn/Controller/Account/Login.php
+++ app/code/Solveda/LinkedIn/Controller/Account/Login.php
@@ -7,46 +7,82 @@ use Magento\Framework\Controller\ResultFactory;
 use Magento\Customer\Model\Session;
 use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Solveda\LinkedIn\Model\LinkedInData;
 
 class Login extends Action
 {
     protected $customerSession;
+    protected $_linkedInData;
 
     public function __construct(
         Context $context,
         Session $customerSession,
+        LinkedInData $linkedInData,
         CustomerRepositoryInterface $customerRepository
         )
     {
         parent::__construct($context);
         $this->customerSession = $customerSession;
+        $this->_linkedInData = $linkedInData;
         $this->customerRepository = $customerRepository;
     }
 
     public function execute()
     {
-        // LinkedIn authentication
+        try {
+            $userData = $this->getLinkedInUserData();
 
-        $userData = $this->getLinkedInUserData();
+            if (!$userData) {
+                $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+                $resultRedirect->setPath('/');
+                return $resultRedirect;
+            }
 
-        $customerId = $this->getCustomerIdByEmail($userData['emailAddress']);
+            $customerId = $this->getCustomerIdByEmail($userData['emailAddress']);
 
-        if ($customerId) {
-            $this->customerSession->setCustomerAsLoggedIn($customerId);
-        } else {
-            $customerId = $this->createCustomerAccount($userData);
-            $this->customerSession->setCustomerAsLoggedIn($customerId);
+            if ($customerId) {
+                $this->customerSession->setCustomerAsLoggedIn($customerId);
+            } else {
+                $customerId = $this->createCustomerAccount($userData);
+
+                if ($customerId) {
+                    $this->customerSession->setCustomerAsLoggedIn($customerId);
+                } else {
+                    $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+                    $resultRedirect->setPath('/');
+                    return $resultRedirect;
+                }
+            }
+
+            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $resultRedirect->setPath('/');
+            return $resultRedirect;
+        } catch (\Exception $e) {
+            $this->messageManager->addError(__('An error occurred during LinkedIn login.'));
+            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $resultRedirect->setPath('/');
+            return $resultRedirect;
         }
-        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-        $resultRedirect->setPath('/');
-        return $resultRedirect;
     }
+
 
     protected function getLinkedInUserData()
     {
-        //
-        return $userData;
+        try {
+            $code = $this->getRequest()->getParam('code');
+            $accessToken = $this->_linkedInData->getAccessToken($code);
+            $userData = $this->_linkedInData->getUserData($accessToken);
+
+            return json_decode($userData, true);
+        } catch (\Exception $e) {
+            $this->messageManager->addError(__('Error during LinkedIn authentication.'));
+            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $resultRedirect->setPath('/');
+            return $resultRedirect;
+        }
     }
+
+
 
     protected function getCustomerIdByEmail($email)
     {
@@ -61,9 +97,24 @@ class Login extends Action
 
     protected function createCustomerAccount($userData)
     {
+        if (!isset($userData['emailAddress'])) {
+            $this->messageManager->addError(__('LinkedIn did not provide an email address.'));
+            return null;
+        }
 
-        //
-        
-        return $customerId;
+        $customer = $this->customerRepository->create();
+        $customer->setEmail($userData['emailAddress']);
+        $customer->setFirstname($userData['localizedFirstName']);
+        $customer->setLastname($userData['localizedLastName']);
+        $customer->setGender($userData['gender'] ?? '');
+
+        try {
+            $customer = $this->customerRepository->save($customer);
+            return $customer->getId();
+        } catch (\Exception $e) {
+            $this->messageManager->addError(__('Could not create customer account.'));
+            return null;
+        }
     }
+
 }
